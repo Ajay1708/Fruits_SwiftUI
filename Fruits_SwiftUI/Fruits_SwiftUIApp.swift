ff089a520b133d1f72fd8442525329d1a0846b9b
Venkataparas <venkata@parastechnologies.com>
2022-08-11 22:00:30 +0530

    Initial Commit

---
+++ Fruits_SwiftUI/Fruits_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits_SwiftUIApp.swift
+//  Fruits_SwiftUI
+//
+//  Created by Venkata Ajay Sai (Paras) on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Fruits_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
